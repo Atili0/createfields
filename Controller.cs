d2de917a1ca9e7636df4e2403c3af16d1d490623
Atili0 <atilio.rosas.e@gmail.com>
2017-12-13 13:51:28 +0100

    new project

---
+++ Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DeXrm.CreateField.Controls;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace DeXrm.CreateField
+{
+    public class LController : ILController
+    {
+        private Login _Login;
+
+        public CrmServiceClient ServerConnection { get; set; }
+        public Guid SystemUserConnectedGuid { get; set; }
+
+        public void CreateConnectionDynamics()
+        {
+            _Login = new Login();
+            _Login.ShowDialog();
+
+        }
+    }
+}
---
+++ IController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace DeXrm.CreateField
+{
+    public interface ILController
+    {
+        void CreateConnectionDynamics();
+        CrmServiceClient ServerConnection { get; set; }
+        Guid SystemUserConnectedGuid { get; set; }
+    }
+}
